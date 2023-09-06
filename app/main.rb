c7ef5f6a4981ffce2573fd3c8fada07c1997e3f9
Anthea Ip <antheaip@Antheas-MBP.hsd1.or.comcast.net>
2023-09-06 09:11:40 -0700

    Recommented main

--- app/main.rb
+++ app/main.rb
@@ -3,10 +3,10 @@ command = ARGV[1]
 
 if command == ".dbinfo"
   File.open(database_file_path, "rb") do |database_file|
-    You can use print statements as follows for debugging, they'll be visible when running tests.
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
     puts "Logs from your program will appear here"
 
-    Uncomment this to pass the first stage
+    # Uncomment this to pass the first stage
     database_file.seek(16)  # Skip the first 16 bytes of the header
     page_size = database_file.read(2).unpack("n")[0]
     puts "database page size: #{page_size}"
